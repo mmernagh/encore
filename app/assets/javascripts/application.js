2ce4533f7ee93586e08418a9c82944c6957dbf7f
mernagh <m@mernagh.com>
2014-12-06 23:24:17 -0500

    add time dropdown and custom time ranges

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui/datepicker
 //= requre home
 //= require turbolinks
 //= require_tree .
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,6 +6,34 @@ function display_historical_options() {
 	}
 }
 
+function display_custom_times() {
+	if ($('#time_select').val() == 6) {
+		$('#custom_time').show();
+	} else {
+		$('#custom_time').hide();
+	}
+}
+
+function restrict_end_date() {
+	if (new Date($('#date_end').val()) < new Date($('#date_start').val())) {
+		$('#date_end').val($('#date_start').val());
+	}
+	if (new Date($('#date_end').val()) == new Date($('#date_start').val()) && 
+		new Date('May 1, 2000' + $('#time_end').val()) < new Date('May 1, 2000' + $('#time_start').val())) {
+		$('#time_end').val($('#time_start').val());
+	}
+}
+
+function restrict_start_date() {
+	if (new Date($('#date_start').val()) > new Date($('#date_end').val())) {
+		$('#date_start').val($('#date_end').val());
+	}
+	if (new Date($('#date_end').val()) == new Date($('#date_start').val()) && 
+		new Date('May 1, 2000' + $('#time_start').val()) > new Date('May 1, 2000' + $('#time_end').val())) {
+		$('#time_start').val($('#time_end').val());
+	}
+}
+
 jQuery(function(){
     // For each radio button whose name is 'chart_type':
     jQuery("input[type='radio'][name='chart_type']").each(function(index, button){
@@ -20,4 +48,45 @@ jQuery(function(){
             display_historical_options();
         });
     });
+    jQuery("select[name='time_select']").each(function(index, button){
+        // Give the button a certain click behaviour:
+        jQuery(button).click(function(){
+            display_custom_times();
+        });
+    });
+    jQuery("input[type='date'][name='date[start]']").each(function(index, button){
+        // Give the button a certain click behaviour:
+        jQuery(button).focusout(function(){
+            restrict_end_date();
+        });
+    });
+    jQuery("input[type='time'][name='time[start]']").each(function(index, button){
+        // Give the button a certain click behaviour:
+        jQuery(button).focusout(function(){
+            restrict_end_date();
+        });
+    });
+    jQuery("input[type='date'][name='date[end]']").each(function(index, button){
+        // Give the button a certain click behaviour:
+        jQuery(button).focusout(function(){
+            restrict_start_date();
+        });
+    });
+    jQuery("input[type='time'][name='time[end]']").each(function(index, button){
+        // Give the button a certain click behaviour:
+        jQuery(button).focusout(function(){
+            restrict_start_date();
+        });
+    });
+    /*
+    jQuery("input[type='date']").each(function(index, button){
+        // Give the button a certain click behaviour:
+        jQuery(button).datepicker({
+        	defaultDate: 0,
+        	maxDate: 0,
+        	// TODO: update this before production, remember that month is zero based
+        	minDate: new Date(2014, 11, 6)
+        });
+    });
+	*/
 });
